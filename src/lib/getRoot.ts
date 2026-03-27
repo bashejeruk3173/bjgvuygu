ebde35b46cf85a420c6528beb7b9f6a69d784e56
Basjeruk Hgse <basahjeruk317@gmail.com>
2026-03-27 20:22:20 +0500

    Update getRoot.ts

--- src/lib/getRoot.ts
+++ src/lib/getRoot.ts
@@ -4,23 +4,9 @@ import { isSiteReachable } from "./isSiteReachable";
 
 // TODO: make config json files , make it better in future
 export const getRoot: RequestHandler = async (_req, res) => {
-  try {
-    const data: GetRoot = {
-      docs: "",
-      sites: {},
-    };
-
-    data.docs = "..";
-
-    const aniwatchStatus = await isSiteReachable("https://aniwatch.to");
-    const aniwatchtvStatus = await isSiteReachable("https://aniwatchtv.to");
-
-    const gogoanimeStatus = await isSiteReachable("https://gogoanime3.co");
-
-    data.sites["aniwatch"] = aniwatchStatus || aniwatchtvStatus;
-
-    res.status(200).json(data);
-  } catch (error) {
-    console.log(error);
-  }
+  res.status(418).json({
+    status: "critical_weeb_overload",
+    message: "The digital hamsters powering this endpoint have abandoned their wheels to watch a 100-episode tournament arc. Please try again after the protagonist unlocks their hidden power.",
+    docs: "Written in the ancient texts, guarded by a very angry tsundere."
+  });
 };
